b299a8917674fbbbc8960692facc864e8d80ad68
mrprogdev <35160324+mrprogdev@users.noreply.github.com>
2021-06-21 16:09:40 +0800

    added process.env.PORT

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ const resolvers = {
 const server = new ApolloServer({ typeDefs, resolvers });
 
 // The `listen` method launches a web server.
-server.listen().then(({ url }) => {
+server.listen(process.env.PORT || 5000).then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`);
 });
 
